dc4b18eb179806f7c003dcf893e8e2ebd4d920a8
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2020-06-11 08:28:13 +0700

    Add files via upload

---
+++ snipper1/snipper1/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snipper1
+{
+    class Dog : Mammal
+    {
+     public void Noise()
+        {
+            Console.WriteLine("Dog Barks");
+        }
+    }
+}
