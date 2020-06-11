4dacb31ff0c5555a93ecaf5bf7df5ef7c43e007f
ha van vu <62437220+HaVanVu1708@users.noreply.github.com>
2020-06-11 08:18:46 +0700

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snipper1
+{
+    class Animal
+    {
+        protected string Food;
+        protected string Activity;
+        public void Eat()
+        {
+            Console.WriteLine("Everyanimal eats something.");
+        }
+        public void DoSomething()
+        {
+            Console.WriteLine("Every animal does something.");
+        }
+        public void Main1()
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snipper1
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            //Dog objDog = new Dog();
+            //objDog.Eat();
+            //objDog.Feature();
+            //objDog.Noise();
+            //Console.ReadLine();
+            //
+            //Cat objCat = new Cat();
+            //objCat.Meo();
+            //objCat.DoSomething();
+            //Console.ReadLine();
+            //
+            Cat objcat = new Cat();
+            objcat.Food = "Mouse";
+            objcat.Activity = "laze around";
+            Console.WriteLine("The Cat loves to eat " + objcat.Food + ".");
+            Console.WriteLine("The Cat loves to " + objcat.Activity + ".");
+            Console.ReadLine();
+        }
+    }
+}
